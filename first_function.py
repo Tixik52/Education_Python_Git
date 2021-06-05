41c2dc8fac5bf8bf801515ce7f6b144c061b1e0c
Anatoly <potapov.anatoly@gmail.com>
2021-06-05 18:23:32 +0300

    change print

--- first_function.py
+++ first_function.py
@@ -7,7 +7,7 @@
 def print_hi(user):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {user}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("End")
+    print("print1")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':

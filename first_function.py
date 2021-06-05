7474cef690d72f9d8f6b5b5b4dba7bab09ca997f
Anatoly <potapov.anatoly@gmail.com>
2021-06-05 18:19:47 +0300

    init

---
+++ first_function.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(user):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {user}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("End")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Enter your name:")
+    user = input()
+    print_hi(user)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
